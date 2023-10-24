27395a4f838076347b4212d07f72e7b90a531b07
frannav <frannavmart@gmail.com>
2023-10-24 21:48:40 +0100

    add export to app and server in order to stop the server and test

---
+++ packages/link-shortener-api/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+const chechBody = "OK";
+
+app.get("/check", async (req, res) => {
+	res.status(200).json({ status: chechBody });
+});
+
+const server = app.listen(port, () => {
+	console.log(`La aplicación está escuchando en el puerto ${port}`);
+});
+
+export { app, server };
