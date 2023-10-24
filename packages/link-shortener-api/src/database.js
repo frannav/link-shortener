847c2c03ae875ee5f4d0acbd438d696ff4b27dcc
frannav <frannavmart@gmail.com>
2023-10-24 23:29:30 +0100

    create simple database with json in order to avoid more initial complexity

---
+++ packages/link-shortener-api/src/database.js
@@ -0,0 +1,37 @@
+import { promises as fs } from 'fs';
+
+export const loadData = async () => {
+  try {
+    const dataBuffer = await fs.readFile('database.json', 'utf-8');
+    return JSON.parse(dataBuffer);
+  } catch (error) {
+    return { urls: [] };
+  }
+};
+
+export const saveData = async (data) => {
+  try {
+    const dataJSON = JSON.stringify(data);
+    await fs.writeFile('database.json', dataJSON);
+  } catch (error) {
+    throw new Error('No se pudo guardar los datos');
+  }
+};
+
+export const addURL = async (newURL) => {
+  const data = await loadData();
+  data.urls.push(newURL);
+  await saveData(data);
+};
+
+// example purpose only
+const main = async () => {
+  try {
+    await addURL("ola1");
+    await addURL("ola2");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
