4323cbe6a6f4a8ed980e908665c9a57a2c702cc5
frannav <frannavmart@gmail.com>
2023-11-05 20:45:20 +0000

    refactor json crud db

--- packages/api/src/database.js
+++ packages/api/src/database.js
@@ -1,39 +1,99 @@
-// import { promises as fs } from "fs";
 const fs = require("fs").promises;
 
 const loadData = async () => {
 	try {
 		const dataBuffer = await fs.readFile("database.json", "utf-8");
+		console.log(dataBuffer);
 		return JSON.parse(dataBuffer);
 	} catch (error) {
-		return { urls: [] };
+		console.error(error);
+		return { data: [] };
 	}
 };
 
-const saveData = async (data) => {
+const loadById = async (id) => {
 	try {
+		const dataBuffer = await fs.readFile("database.json", "utf-8");
+		const jsonData = JSON.parse(dataBuffer);
+		console.log(jsonData);
+		const result = jsonData.data.find((item) => item === id);
+		if (result) {
+			console.log(result);
+			return {
+				status: "OK",
+				data: result,
+			};
+		}
+		return {
+			status: "Not Found",
+		};
+	} catch (error) {
+		console.error(error);
+		throw new Error(`Error searching data. Error:${error}`);
+	}
+};
+
+const saveData = async (newItem) => {
+	try {
+		const data = await loadData();
+		data.data.push(newItem);
 		const dataJSON = JSON.stringify(data);
 		await fs.writeFile("database.json", dataJSON);
+		return {
+			status: "OK",
+			savedData: newItem,
+		};
 	} catch (error) {
-		throw new Error("No se pudo guardar los datos");
+		console.error(error);
+		throw new Error(`Error saving data. Error:${error}`);
 	}
 };
 
-const addURL = async (newURL) => {
-	const data = await loadData();
-	data.urls.push(newURL);
-	await saveData(data);
+const deleteData = async (itemToDelete) => {
+	try {
+		const data = await loadData();
+		const filteredURLs = data.data.filter((item) => item !== itemToDelete);
+		data.urls = filteredURLs;
+		// await saveData(data);
+		const dataJSON = JSON.stringify(data);
+		await fs.writeFile("database.json", dataJSON);
+		return {
+			status: "OK",
+			deletedItem: itemToDelete,
+		};
+	} catch (error) {
+		console.error(error);
+		throw new Error(`Error deleting data. Error:${error}`);
+	}
 };
+
+const deleteAll = async () => {
+	try {
+		const data = { data: [] };
+		const dataJSON = JSON.stringify(data);
+		await fs.writeFile("database.json", dataJSON);
+		return {
+			status: "OK",
+		};
+	} catch (error) {
+		console.error(error);
+		throw new Error(`Error deleting data. Error:${error}`);
+	}
+};
+
 
 // example purpose only
 // const main = async () => {
 // 	try {
-// 		await addURL("ola1");
-// 		await addURL("ola2");
+// 		await saveData("data1");
+// 		await saveData("data2");
+// 		await saveData("data3");
+// 		await deleteAll();
+// 		await loadById("");
 // 	} catch (error) {
 // 		console.error(error);
 // 	}
 // };
 
 // main();
-module.exports = { loadData, saveData, addURL };
+module.exports = { loadData, saveData, deleteData, deleteAll, loadById };
